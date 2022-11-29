3312fa3188b3be8042ba1758fe3ff74853a03444
Polina <posinka@mail.ru>
2022-11-29 13:13:47 +0300

    2.1

--- 1/Program.cs
+++ 1/Program.cs
@@ -2,18 +2,18 @@
 // (A и B) и возводит число A в натуральную степень B.
 
 Console.WriteLine ("Укажите число A ");
-double A = Convert.ToInt32(Console.ReadLine());
+int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Укажите число B");
 int B = Convert.ToInt32(Console.ReadLine());
 
-double Power(double num1, int num2)
+int Power(int num1, int num2)
 {
-    double result = 1;
-    for (int i=0; i<num2; i++)
+    int result = 1;
+    for (int i=0; i<=num2; i++)
     {
         result *= num1;
     }
 return result;
 }
-double res = Power(A,B);
+int res = Power(A,B);
 Console.WriteLine(res);
