de0d85acb8aa56c5b6ed27c9c4f1e9cbf19a4e9f
Polina <posinka@mail.ru>
2022-11-29 12:22:46 +0300

    3

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+
+int [] GetArray (int size)
+{
+    int[] array = new int [size];
+    for (int i=0; i<size; i++)
+    {
+        array[i]=new Random().Next(0,100);
+    }
+    return array;
+}
+int[] array= GetArray(8);
+
+Console.Write(String.Join(" ", array));
